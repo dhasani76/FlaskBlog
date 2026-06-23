6a835fed19302f75067cbfcdd095d220030773e3
dhasani76 <dhasani76@outlook.com>
2026-06-23 01:30:00 -0400

    Updated Main and Contact

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def load_user(user_id):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -269,4 +269,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
