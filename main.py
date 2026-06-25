5633b047bdd36beff5471e541c9adfe0b1ddb743
dhasani76 <dhasani76@outlook.com>
2026-06-25 00:34:36 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
-# Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 '''
